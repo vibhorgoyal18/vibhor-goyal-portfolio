1ba0fa2f76902476e770da84081a7be2afaa3838
VIBHOR GOYAL <goyal1992.vibhor@gmail.com>
2019-07-26 15:45:26 +0530

    Modified footer width height

--- src/app/modules/footer/footer.component.ts
+++ src/app/modules/footer/footer.component.ts
@@ -1,19 +1,33 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {element} from 'protractor';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, AfterViewInit {
 
   latitude = 28.5769245;
   longitude = 77.391029;
+  height = '400px';
+  @ViewChild('footerText', {static: false}) text: ElementRef;
+
 
   constructor() {
   }
 
   ngOnInit() {
   }
+
+  ngAfterViewInit(): void {
+    if (window.innerWidth > 767) {
+      this.height = this.text.nativeElement.offsetHeight + 'px';
+    } else {
+      this.height = '400px';
+    }
+
+  }
+
 
 }
